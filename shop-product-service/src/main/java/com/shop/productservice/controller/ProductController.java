ea1c6b812ecf3e7925b07d31c31bda06010ea071
prashant <prashant.aghara2111@gmail.com>
2023-09-30 16:54:24 +0530

    INITIAL SETUP & PRODUCT SERVICE

---
+++ shop-product-service/src/main/java/com/shop/productservice/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.shop.productservice.controller;
+
+import com.shop.productservice.dto.ProductRequest;
+import com.shop.productservice.dto.ProductResponse;
+import com.shop.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getProduct() {
+        return productService.getAllProducts();
+    }
+}
---
+++ shop-product-service/src/main/java/com/shop/productservice/dto/ProductRequest.java
@@ -0,0 +1,18 @@
+package com.shop.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
